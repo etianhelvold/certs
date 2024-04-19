3716330bf9f890a5bb0ec297fbfa9e937ff25d17
Etian Helvold <joao.etian.iglesias@gmail.com>
2024-04-19 15:55:34 +0100

    More lessons and readme fixes

---
+++ FundamentalsofC++/UsingVariablesAndDatatypes/variables_and_initialization.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    int balance; // Declaring a variable
+
+    cout << "My account balance is: $" << balance << endl;
+
+    balance = 50000;
+
+    cout << "My account balance is: $" << balance << endl;
+
+    return 0;
+}   
