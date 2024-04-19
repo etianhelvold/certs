fa7042da2b2be92d4f579981c2331ef130662582
Etian Helvold <joao.etian.iglesias@gmail.com>
2024-04-19 17:59:01 +0100

    vs code shenanigans

---
+++ FundamentalsofC++/UsingVariablesAndDatatypes/Introducing C++ Variables/warning_narrowing_conversion_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    //Assignment notation;
+    int balance(1000.45); //will truncate
+    int withdrawal = 200;
+    int final_balance = balance - withdrawal;
+
+    cout << "My current account balance is: $" << balance << endl;
+    cout << "I need to withdraw: $" << withdrawal << endl;
+    cout << "My final account balance is: $" << final_balance << endl;
+
+    return 0;
+}   
